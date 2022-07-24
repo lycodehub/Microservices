ef188d0d9714d1f15bae74fa2bd1286c2081fce9
lylyly <lengyan175@qq.com>
2022-07-24 13:03:35 +0800

    first commit

---
+++ 01-cloud-demo/user-service/src/main/java/cn/ly00/user/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package cn.ly00.user.mapper;
+
+import cn.ly00.user.pojo.User;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+public interface UserMapper {
+    
+    @Select("select * from tb_user where id = #{id}")
+    User findById(@Param("id") Long id);
+}
